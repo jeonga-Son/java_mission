f306a3a263ddae94838a58191e6def31d0cdce48
jeonga-Son <jjeong0106@gmail.com>
2023-02-03 22:35:09 +0900

    question1-1 answer

--- java_mission/src/mission_1/question1_1.java
+++ java_mission/src/mission_1/question1_1.java
@@ -3,8 +3,17 @@ package mission_1;
 public class question1_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int number = 5;
+		int idx = 1;
+		
+		for(int i = 1; i <= number; i++) {
+			for(int j = idx; j <= i * number; j++) {
+				System.out.printf("%3d", j);
+			}
+			
+			System.out.println();
+			
+			idx += number;
+		}
 	}
-
 }
