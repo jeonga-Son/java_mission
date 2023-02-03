a5369f8fb5240d300399923ff8a2a58fe88a0eb1
jeonga-Son <jjeong0106@gmail.com>
2023-02-04 00:16:18 +0900

    question2_1 answer

---
+++ java_mission/src/mission_1/quesiont2_1.java
@@ -0,0 +1,17 @@
+package mission_1;
+
+public class quesiont2_1 {
+
+	public static void main(String[] args) {
+		int number = 5;
+		String str = "*";
+		
+		for (int i = 0; i < number; i++) {
+			for (int j = 0; j < number; j++) {
+				System.out.print(str);
+			}
+			
+			System.out.println();
+		}
+	}
+}
